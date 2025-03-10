c2f37b180bee67ba50df15a3feb58d09c225d7b9
jackmounts <giacomo.monti9@icloud.com>
2025-03-10 15:30:16 +0100

    Add 3D model integration to login page; implement canvas and lighting components

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,61 +1,90 @@
 "use client";
+import { Common, Card } from "@/utils/models";
+import { OrbitControls, Preload, View } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const LoginPage: React.FC = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const [element, setElement] = useState<HTMLElement>();
+
+  /**
+   * Set the root element
+   */
+  useEffect(() => {
+    const root = document.getElementById("login-root");
+    if (root) {
+      setElement(root);
+    }
+  }, []);
+
   return (
-    <div className="flex flex-row items-center justify-center size-full bg-[url('/images/login_bg.svg')] bg-cover bg-center">
-      <div className="hidden lg:flex w-3/5">
-        {/* https://sketchfab.com/3d-models/trading-card-blank-low-poly-798f5a9b46bb439ca413c40e7938a6ed */}
-      </div>
-      <div className="flex flex-col w-full lg:w-2/5 items-center justify-start gap-y-4">
-        <div className="flex flex-col items-center justify-center w-full text-white font-semibold mb-10">
-          <div className="flex w-9/10 lg:w-7/10 text-3xl lg:text-6xl">
-            <p>Welcome back,</p>
+    <>
+      <div
+        className="flex flex-row items-center justify-center size-full bg-[url('/images/login_bg.svg')] bg-cover bg-center"
+        id="login-root"
+      >
+        <div className="hidden lg:flex w-3/5">
+          {/* https://sketchfab.com/3d-models/trading-card-blank-low-poly-798f5a9b46bb439ca413c40e7938a6ed */}
+          <View className="view">
+            <Common />
+            <Card position={[0, -1, 0]} scale={15} />
+            <OrbitControls makeDefault enableZoom={false} enablePan={false} />
+          </View>
+        </div>
+        <div className="flex flex-col w-full lg:w-2/5 items-center justify-start gap-y-4">
+          <div className="flex flex-col items-center justify-center w-full text-white font-semibold mb-10">
+            <div className="flex w-9/10 lg:w-7/10 text-3xl lg:text-4xl">
+              <p>Welcome back,</p>
+            </div>
+            <div className="flex w-9/10 lg:w-7/10 text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl">
+              <p>Planeswalker</p>
+            </div>
           </div>
-          <div className="flex w-9/10 lg:w-7/10 text-5xl lg:text-8xl">
-            <p>Planeswalker</p>
+          <div className="flex w-9/10 lg:w-7/10">
+            <input
+              className="w-full h-12 bg-white rounded-lg px-4 placeholder:italic placeholder:text-gray-500"
+              type="email"
+              id="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
           </div>
-        </div>
-        <div className="flex w-9/10 lg:w-7/10">
-          <input
-            className="w-full h-12 bg-white rounded-lg px-4 placeholder:italic placeholder:text-gray-500"
-            type="email"
-            id="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div className="flex w-9/10 lg:w-7/10">
-          <input
-            className="w-full h-12 bg-white rounded-lg px-4 placeholder:italic placeholder:text-gray-500"
-            type="password"
-            id="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <div className="flex w-9/10 lg:w-7/10">
-          <Link href={"/"} className="w-full">
-            <button className="bg-gray-800 text-white rounded-2xl px-4 py-2 w-full h-16 font-semibold hover:bg-gray-900">
-              Log In
+          <div className="flex w-9/10 lg:w-7/10">
+            <input
+              className="w-full h-12 bg-white rounded-lg px-4 placeholder:italic placeholder:text-gray-500"
+              type="password"
+              id="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div className="flex w-9/10 lg:w-7/10">
+            <Link href={"/"} className="w-full">
+              <button className="bg-gray-800 text-white rounded-2xl px-4 py-2 w-full h-16 font-semibold hover:bg-gray-900">
+                Log In
+              </button>
+            </Link>
+          </div>
+          <div className="flex w-9/10 lg:w-7/10">
+            <button className="bg-gray-200 text-gray-900 rounded-2xl px-4 py-2 w-full h-16 font-semibold hover:bg-gray-900 hover:text-white">
+              Register
             </button>
-          </Link>
-        </div>
-        <div className="flex w-9/10 lg:w-7/10">
-          <button className="bg-gray-200 text-gray-900 rounded-2xl px-4 py-2 w-full h-16 font-semibold hover:bg-gray-900 hover:text-white">
-            Register
-          </button>
+          </div>
         </div>
       </div>
-    </div>
+      <Canvas className="canvas" eventSource={element}>
+        <View.Port />
+        <Preload all />
+      </Canvas>
+    </>
   );
 };
 
---
+++ src/utils/models.tsx
@@ -0,0 +1,28 @@
+import { Environment, PerspectiveCamera, useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export function Common({ color }: { color?: string }) {
+  return (
+    <>
+      {color && <color attach="background" args={[color]} />}
+      <ambientLight intensity={0.5} />
+      <pointLight position={[-20, 30, 10]} intensity={5} />
+      <pointLight position={[-10, -10, -10]} color="white" intensity={3} />
+      <Environment preset="studio" />
+      <PerspectiveCamera makeDefault fov={35} position={[0, 1, 6]} />
+    </>
+  );
+}
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export function Card(props: any) {
+  const { scene } = useGLTF(
+    "/models/d382c62a8f56431beea90aad46a74c2c1d6b17c1.glb"
+  );
+  useFrame((state, delta) => {
+    scene.rotation.y += delta * 0.5;
+    scene.rotation.z = 0.1;
+  });
+  return <primitive object={scene} {...props} />;
+}
