105f4289afbbc3d24694570d29918ffab2ac261a
jackmounts <giacomo.monti9@icloud.com>
2025-03-11 14:21:36 +0100

    Add SVG icons and create collection, profile, and scanner pages; enhance header with dropdowns

---
+++ src/app/pages/collection/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CollectionPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Collection Page</h1>
+      <p>Welcome to the collection page!</p>
+    </div>
+  );
+};
+
+export default CollectionPage;
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,15 @@
 "use client";
 import Image from "next/image";
+import Link from "next/link";
 import React, { useEffect, useRef } from "react";
-import searchImage from "../../../public/icons/search.svg";
-import accountImage from "../../../public/icons/account_circle.svg";
-import dropdownImage from "../../../public/icons/arrow_drop_down.svg";
+import searchIcon from "../../../public/icons/search.svg";
+import accountIcon from "../../../public/icons/account_circle.svg";
+import dropdownIcon from "../../../public/icons/arrow_drop_down.svg";
+import homeIcon from "../../../public/icons/home.svg";
+import collectionIcon from "../../../public/icons/library.svg";
+import addIcon from "../../../public/icons/add.svg";
+import personIcon from "../../../public/icons/person.svg";
+import logoutIcon from "../../../public/icons/logout.svg";
 
 const Header: React.FC = () => {
   return (
@@ -13,30 +19,23 @@ const Header: React.FC = () => {
           <input
             type="text"
             placeholder="Sol Ring"
-            className="px-2 py-1 placeholder:text-gray-500 placeholder:italic text-gray-800 focus-within:outline-none"
+            className="px-4 py-1 w-full placeholder:text-gray-500 placeholder:italic text-gray-800 focus-within:outline-none"
           />
           <Image
-            src={searchImage}
+            src={searchIcon}
             alt={"Search image"}
             className="absolute right-0 invert me-2"
             onClick={() => console.log("Search")}
           />
         </div>
-        <Dropdown />
-        <div className="max-md:hidden">
-          <Image
-            src={accountImage}
-            alt={"Account image"}
-            className="size-8 hover:scale-110 hover:cursor-pointer"
-            onClick={() => console.log("Profile")}
-          />
-        </div>
+        <DropdownExplore />
+        <DropdownProfile />
       </div>
     </header>
   );
 };
 
-const Dropdown: React.FC = () => {
+const DropdownExplore: React.FC = () => {
   const [isDropdownOpen, setIsDropdownOpen] = React.useState(false);
   const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
   const dropdownRef = useRef<HTMLDivElement>(null);
@@ -57,23 +56,91 @@ const Dropdown: React.FC = () => {
   }, []);
 
   return (
-    <div className="relative  inline-block max-md:hidden" ref={dropdownRef}>
+    <div className="relative inline-block max-md:hidden" ref={dropdownRef}>
       <div
         className="flex flex-row justify-center items-center hover:cursor-pointer hover:font-semibold"
         onClick={toggleDropdown}
       >
         <div className="text-white text-lg">Explore</div>
+        <Image src={dropdownIcon} alt={"Dropdown image"} />
+      </div>
+      {isDropdownOpen && (
+        <ul className="absolute top-full left-0 px-8 py-6 z-50 bg-white dark:bg-[var(--background)] rounded-2xl shadow-lg min-w-max">
+          <li className="flex flex-row justify-start items-center gap-1 hover:font-bold cursor-pointer">
+            <Image src={homeIcon} alt={"Home icon"} className="invert" />
+            <Link href={"/"} onClick={toggleDropdown}>
+              Home
+            </Link>
+          </li>
+          <li className="flex flex-row justify-start items-center gap-1 hover:font-bold cursor-pointer py-2">
+            <Image
+              src={collectionIcon}
+              alt={"Collection icon"}
+              className="invert"
+            />
+            <Link href={"/collection"} onClick={toggleDropdown}>
+              Collection
+            </Link>
+          </li>
+          <li className="flex flex-row justify-start items-center gap-1 hover:font-bold cursor-pointer">
+            <Image src={addIcon} alt={"Scanner icon"} className="invert" />
+            <Link href={"/scanner"} onClick={toggleDropdown}>
+              Scanner
+            </Link>
+          </li>
+        </ul>
+      )}
+    </div>
+  );
+};
+
+const DropdownProfile: React.FC = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = React.useState(false);
+  const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsDropdownOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div className="relative inline-block max-md:hidden" ref={dropdownRef}>
+      <div
+        className="flex flex-row justify-center items-center hover:cursor-pointer hover:font-semibold"
+        onClick={toggleDropdown}
+      >
         <Image
-          src={dropdownImage}
-          alt={"Dropdown image"}
-          onClick={() => console.log("Dropdown")}
+          src={accountIcon}
+          alt={"Account image"}
+          className="size-8 hover:scale-110 hover:cursor-pointer"
+          onClick={() => console.log("Profile")}
         />
       </div>
       {isDropdownOpen && (
-        <ul className="absolute top-full left-0 p-8 z-50 bg-white dark:bg-[var(--background)] rounded-2xl shadow-lg min-w-[200px]">
-          <li className="hover:font-bold cursor-pointer">Home</li>
-          <li className="hover:font-bold cursor-pointer py-2">Collections</li>
-          <li className="hover:font-bold cursor-pointer">Scanner</li>
+        <ul className="absolute top-full right-0 py-6 px-8 z-50 bg-white dark:bg-[var(--background)] rounded-2xl shadow-lg min-w-max">
+          <li className="flex flex-row justify-start items-center gap-1 hover:font-bold cursor-pointer">
+            <Image src={personIcon} alt={"Home icon"} className="invert" />
+            <Link href={"/profile"} onClick={toggleDropdown}>
+              Profile
+            </Link>
+          </li>
+          <li className="flex flex-row justify-start items-center gap-1 hover:font-bold cursor-pointer pt-2">
+            <Image src={logoutIcon} alt={"Home icon"} className="invert" />
+            <Link href={"/login"} onClick={toggleDropdown}>
+              Log out
+            </Link>
+          </li>
         </ul>
       )}
     </div>
