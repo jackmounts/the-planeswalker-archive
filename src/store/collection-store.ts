7e37ad0b0c69fb2b898e20afb8729beb78f71dc7
jackmounts <giacomo.monti9@icloud.com>
2025-03-11 11:50:10 +0100

    Add Dexie.js setup and Zustand state management for profiles and collections

---
+++ src/store/collection-store.ts
@@ -0,0 +1,44 @@
+import { create } from "zustand";
+import db, { Collection } from "@/app/db";
+import useProfileStore from "./profile-store";
+
+type CollectionState = {
+  collections: Collection[];
+};
+
+type CollectionAction = {
+  loadCollections: () => Promise<void>;
+  createCollection: (collection: Collection) => Promise<void>;
+  updateCollection: (collection: Collection) => Promise<void>;
+  deleteCollection: (uuid: number) => Promise<void>;
+};
+
+const useCollectionStore = create<CollectionState & CollectionAction>(
+  (set) => ({
+    collections: [],
+
+    loadCollections: async () => {
+      const { logged_profile_uuid } = useProfileStore.getState();
+      const collections = await db.collections
+        .where("profile_uuid")
+        .equals(logged_profile_uuid!)
+        .toArray();
+      set({ collections });
+    },
+
+    createCollection: async (collection) => {
+      await db.collections.add(collection);
+      await useCollectionStore.getState().loadCollections();
+    },
+
+    updateCollection: async (collection) => {
+      await db.collections.update(collection.uuid, collection);
+      await useCollectionStore.getState().loadCollections();
+    },
+
+    deleteCollection: async (uuid) => {
+      await db.collections.delete(uuid);
+      await useCollectionStore.getState().loadCollections();
+    },
+  })
+);
